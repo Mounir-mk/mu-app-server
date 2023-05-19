c0200fde1d41007b4bfb036978dbb04fd706eb6e
Mounir-mk <mounir.mekoui@gmail.com>
2023-05-20 00:59:50 +0200

    setting up back auth

--- prisma/data.js
+++ prisma/data.js
@@ -17,6 +17,12 @@ const users = [
       "$argon2id$v=19$m=16,t=2,p=1$ZTNYU1U2cjRyNUJuTkJzVA$lgBqAYdbLX+j1Bc6DP+WAw",
     role: "chief",
   },
+  {
+    email: "mounir@mail.com",
+    hashed_password:
+      "$argon2id$v=19$m=65536,t=5,p=1$1AW6VtQDnXEXuSAenQS4+g$mUmb4mksj+Dbpwo+ZaIr07Xciocaa7o6zKE8br90xPE",
+    role: "admin",
+  },
 ];
 
 module.exports = {
--- src/router.js
+++ src/router.js
@@ -1,30 +1,42 @@
 const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 const router = express.Router();
 
-const { sendEmail } = require("./services/emailSending");
-
-router.post("/send-email", sendEmail);
-
-router.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-const teamController = require("./controllers/teamControllers");
-
-router.post("/teams", teamController.createTeam);
-router.get("/teams", teamController.browse);
-
 const userController = require("./controllers/userControllers");
 
-const { verifyPassword, hashPassword } = require("./services/auth");
+const {
+  verifyPassword,
+  hashPassword,
+  refreshTokens,
+} = require("./services/auth");
 
 router.post("/users", hashPassword, userController.register);
+router.post("/register", hashPassword, async (req, res) => {
+  const { email, hashedPassword } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      email,
+      hashed_password: hashedPassword,
+      role: "admin",
+    },
+  });
+  res.json(user);
+});
+
 router.get("/users", userController.browse);
 router.post(
   "/users/login",
   userController.getUserByEmailWithPasswordAndPassToNext,
   verifyPassword
 );
+router.post("/token", refreshTokens);
+
+const teamController = require("./controllers/teamControllers");
+
+router.post("/teams", teamController.createTeam);
+router.get("/teams", teamController.browse);
 
 module.exports = router;
